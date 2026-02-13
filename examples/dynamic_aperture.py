d6a0e29356b2594cf435e6b50f44e4d04e2532bb
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-02-13 13:23:34 +0100

    [FIX] correct prefix for topup cc

--- examples/dynamic_aperture.py
+++ examples/dynamic_aperture.py
@@ -4,12 +4,15 @@ import json
 from typing import Dict
 
 import jsons
+import pathlib
 
 from ophyd_async.core import StandardReadable, soft_signal_rw
 
 from bact_bessyii_bluesky.model.excitation_rays import ExcitationCollection
 from bact_bessyii_bluesky.plans.execute_rays import execute_rays_plan
-from bact_bessyii_bluesky.plans.reinjection_diagnostic_kicker_disabled import setup_reinjection
+from bact_bessyii_bluesky.plans.reinjection_diagnostic_kicker_disabled import (
+    setup_reinjection,
+)
 from bact_bessyii_ophyd_async.devices.pp.kicker import Kicker
 import bluesky
 import bluesky.plans as bp
@@ -21,14 +24,12 @@ from bact_bessyii_ophyd_async.devices.pp.topup_engine import TopUpEngine
 
 
 class DAMeasure(StandardReadable):
-    def __init__(self, name:str):
+    def __init__(self, name: str):
         with self.add_children_as_readables():
             self.hk = Kicker(ps_prefix="PKDHKR:", delay_prefix="KDHKR:", name="hk")
             self.vk = Kicker(ps_prefix="PKDVKR:", delay_prefix="KDVKR:", name="vk")
-            self.topup = TopUpEngine(target_current=5, acceptable_loss=0.5)
+            self.topup = TopUpEngine("TOPUPCC:", target_current=5, acceptable_loss=0.5)
         super().__init__(name=name)
-
-
 
 
 def main(ray_data_file_name: str):
@@ -40,14 +41,17 @@ def main(ray_data_file_name: str):
 
     async def connect():
         await da.connect()
+
     asyncio.run(connect())
 
     RE = bluesky.RunEngine(dict(target="dynamic_aperture"))
 
-    lt = LiveTable([
-        da.hk.ps.setpoint.name,
-        da.vk.ps.setpoint.name,
-    ])
+    lt = LiveTable(
+        [
+            da.hk.ps.setpoint.name,
+            da.vk.ps.setpoint.name,
+        ]
+    )
     RE.subscribe(lt)
 
     info_signals = dict(
@@ -63,13 +67,13 @@ def main(ray_data_file_name: str):
         current = input[da.topup.current.name]["value"]
         return current > min_current
 
-    reinject_plan=setup_reinjection(
+    reinject_plan = setup_reinjection(
         topup_device=da.topup,
         frequency_switcher=da.topup.frq_switch,
         horizontal_kicker_device=da.hk,
         vertical_kicker_device=da.vk,
     )
-    uid, = RE(
+    (uid,) = RE(
         execute_rays_plan(
             detectors=[da],
             horizontal_excitation=da.hk.ps,
@@ -84,6 +88,6 @@ def main(ray_data_file_name: str):
     print(f"{uid=}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     atexit.register(purge_channel_caches)
-    main()
+    main(pathlib.Path(__name__).absolute().parent / "notebooks" / "excitation_data.json")
