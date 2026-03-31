7cacfae06e355a8f106a8530e267e657fd724e2d
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-03-31 11:17:35 +0200

    [TASK] measure dynamic aperture with 2.0 A, appropriate bpms selected

--- examples/dynamic_aperture.py
+++ examples/dynamic_aperture.py
@@ -1,10 +1,12 @@
 import asyncio
 import atexit
+import time
 import json
 from typing import Dict
 
 import jsons
 import pathlib
+import pprint
 
 from ophyd_async.core import StandardReadable, soft_signal_rw
 
@@ -19,6 +21,7 @@ import bluesky.plans as bp
 from bluesky.protocols import Reading
 from bluesky.callbacks import LiveTable
 from aioca import purge_channel_caches
+from databroker import catalog
 
 from bact_bessyii_ophyd_async.devices.pp.topup_engine import TopUpEngine
 from bact_bessyii_mls_ophyd.devices.pp.bpm_collection import BPMCollection
@@ -30,7 +33,7 @@ class DAMeasure(StandardReadable):
         with self.add_children_as_readables():
             self.hk = Kicker(ps_prefix="PKDHKR:", delay_prefix="KDHKR:", name="hk")
             self.vk = Kicker(ps_prefix="PKDVKR:", delay_prefix="KDVKR:", name="vk")
-            self.topup = TopUpEngine("TOPUPCC:", target_current=5, acceptable_loss=0.5)
+            self.topup = TopUpEngine("TOPUPCC:", target_current=2, acceptable_loss=0.5)
         super().__init__(name=name)
 
 
@@ -38,14 +41,16 @@ def main(ray_data_file_name: str):
     with open(ray_data_file_name, "rt") as fp:
         tmp = json.load(fp)
     excitation_rays = jsons.load(tmp, ExcitationCollection)
-
+    # just the first five
+    excitation_rays = ExcitationCollection(col=excitation_rays.col[:10])
     da = DAMeasure(name="da")
 
     bpm_col = BPMCollection(
         name="bpm",
         devices=[
-            BPM(prefix="BPMZ41D1R:", name="bpmz41", select_slice=slice(0, 2000)),
-            BPM(prefix="BPMZ42D1R:", name="bpmz42", select_slice=slice(0, 2000)),
+            BPM(prefix="BPMZ1D8R:", name="bpmz1d8r", select_slice=slice(0, 2048)),
+            BPM(prefix="BPMZ4D8R:", name="bpmz4d8r", select_slice=slice(0, 2048)),
+            BPM(prefix="BPMZ5D8R:", name="bpmz5d8r", select_slice=slice(0, 2048)),
         ],
         combine_suffixes=["tbt"],
     )
@@ -62,10 +67,15 @@ def main(ray_data_file_name: str):
     lt = LiveTable(
         [
             da.hk.ps.setpoint.name,
+            da.hk.ps.readback.name,
             da.vk.ps.setpoint.name,
+            da.vk.ps.readback.name,
+            da.topup.current.name,
         ]
     )
     RE.subscribe(lt)
+    db = catalog["heavy"]
+    RE.subscribe(db.v1.insert)
 
     info_signals = dict(
         ray=soft_signal_rw(int, name="ray"),
@@ -95,6 +105,7 @@ def main(ray_data_file_name: str):
             rays=excitation_rays,
             reinject_plan=reinject_plan,
             go_on=go_on,
+            n_samples=1,
         )
     )
 
@@ -102,5 +113,7 @@ def main(ray_data_file_name: str):
 
 
 if __name__ == "__main__":
-    atexit.register(purge_channel_caches)
-    main(pathlib.Path(__name__).absolute().parent / "notebooks" / "excitation_data.json")
+    # atexit.register(purge_channel_caches)
+    main(pathlib.Path(__name__).absolute().parent / "examples" / "notebooks" / "excitation_data.json")
+    # a chance to sleep a bit
+    time.sleep(2.0)
